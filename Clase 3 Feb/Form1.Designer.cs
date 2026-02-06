46c2df8567bb94c131d5e841964909e95e14880f
israe <israe@LAPTOP-2IP5L4C8>
2026-02-06 10:33:20 -0600

    Agregar archivos de proyecto.

---
+++ Clase 3 Feb/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Clase_3_Feb
+{
+    partial class Bloc
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GuardarComo = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GuardarComo,
+            this.salirToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem1});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // GuardarComo
+            // 
+            this.GuardarComo.Name = "GuardarComo";
+            this.GuardarComo.Size = new System.Drawing.Size(189, 26);
+            this.GuardarComo.Text = "Guardar Como";
+            this.GuardarComo.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem1
+            // 
+            this.guardarToolStripMenuItem1.Name = "guardarToolStripMenuItem1";
+            this.guardarToolStripMenuItem1.Size = new System.Drawing.Size(189, 26);
+            this.guardarToolStripMenuItem1.Text = "Guardar";
+            this.guardarToolStripMenuItem1.Click += new System.EventHandler(this.guardarToolStripMenuItem1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 396);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Bloc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Bloc";
+            this.Text = "Bloc de Notas";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GuardarComo;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem1;
+    }
+}
+
---
+++ Clase 3 Feb/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //Bilioteca para el manerjo de archivos
+
+namespace Clase_3_Feb
+{
+    public partial class Bloc : Form
+    {
+        private string file = null;
+        public Bloc()
+        {
+            InitializeComponent();
+            toolStripStatusLabel1.Text = "Escribiendo";
+        }
+
+        //GUARDAR COMO
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog(); //Abrir cuadro de dailogo ppara seleccioanr una ruta
+            //Especficar el tipo de archivo que vamos a guardar
+            sfd.Filter = "Docuentos de Text |*.txt";
+            sfd.AddExtension = true;
+            DialogResult res = sfd.ShowDialog();//Veridica DIALOGResut para devolder una validacion   //Muestra el cuado de dialgooo
+
+
+            //Para que no truene por no sleccionar una direccion agregamos una validacion para comporbar que Dialog result es Ok
+            if (res == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, textBox1.Text);
+                file = sfd.FileName;
+            }
+            ///Guardar archivo en sfd.FileName que da la direccion seleccionada por el cuadro
+            //de dialogo, siendo uno la direccion de guardar y lo otro que es lo que se va a escribir
+
+            toolStripStatusLabel1.Text = "Guardado";
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog sfd = new OpenFileDialog(); //Abrir cuadro de dailogo ppara seleccioanr una ruta
+            //Especficar el tipo de archivo que vamos a guardar
+            sfd.Filter = "Docuentos de Text |*.txt";
+            sfd.AddExtension = true;
+            DialogResult res = sfd.ShowDialog();//Veridica DIALOGResut para devolder una validacion   //Muestra el cuado de dialgooo
+
+
+            //Para que no truene por no sleccionar una direccion agregamos una validacion para comporbar que Dialog result es Ok
+            if (res == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(sfd.FileName);
+                file = sfd.FileName;
+            }
+            //Abrir un archivo y ponerle al cuadro de texto la información de un texto
+            //de dialogo, siendo uno la direccion de guarda y lo otro que es lo que se va a escribir
+
+            toolStripStatusLabel1.Text = "Archivo Abierto";
+        }
+
+        private void guardarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if(file!= null)
+                File.WriteAllText(file, textBox1.Text);
+            
+            else
+           guardarToolStripMenuItem_Click(sender, e);
+
+
+            toolStripStatusLabel1.Text = "Guardado";
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Escribiendo";
+        }
+    }
+}
+//Agrggar pestañas para multiples documentos y que cada pestaña sea un documento diferente y sea independiente
+
+//Rich TextBox para poner emojisy ejemplo si escribimos :) que lo convierta en una carita feliz
+//Todo se guarda como texto simple pero al mostrarlo en el RichTextBox se ve como emoji o imagen
